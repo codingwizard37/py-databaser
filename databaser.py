c6814c323a25ef4919ef225c3578a0b42b36a320
Jake Searle <jacobbsearle@gmail.com>
2020-04-22 22:54:10 -0500

    Streamlined scraping and database insertion

--- databaser.py
+++ databaser.py
@@ -1,3 +1,5 @@
+from urllib.error import HTTPError
+
 from pymongo import MongoClient
 import json
 import os
@@ -8,21 +10,21 @@ from bs4 import BeautifulSoup
 
 # Globals
 book_code_dict = {
-  "1-ne": 22,
-  "2-ne": 33,
-  "jacob": 7,
-  "enos": 1,
-  "jarom": 1,
-  "omni": 1,
-  "w-of-m": 1,
-  "mosiah": 29,
-  "alma": 63,
-  "hel": 16,
-  "3-ne": 30,
-  "4-ne": 1,
-  "morm": 9,
-  "ether": 15,
-  "moro": 10
+    "1-ne": 22,
+    "2-ne": 33,
+    "jacob": 7,
+    "enos": 1,
+    "jarom": 1,
+    "omni": 1,
+    "w-of-m": 1,
+    "mosiah": 29,
+    "alma": 63,
+    "hel": 16,
+    "3-ne": 30,
+    "4-ne": 1,
+    "morm": 9,
+    "ether": 15,
+    "moro": 10
 }
 
 
@@ -89,26 +91,35 @@ def get_verses(url):
     return verse_list
 
 
-def add_new_languages(lang_list):
+def add_new_languages(new_lang):
     # I'm aware of list comprehension, which I could've used to make a list of
     # urls, instead of this triple for loop, but that was causing request issues
     # for some reason.
-    for new_lang in lang_list:
-        for new_book, new_ch_max in book_code_dict.items():
-            for new_ch_num in range(new_ch_max):
-                url = f"https://www.churchofjesuschrist.org/study/scriptures/" \
-                      f"bofm/{new_book}/{new_ch_num + 1}?lang={new_lang} "
-                ch_dict = get_chapter(url)
+    for new_book, new_ch_max in book_code_dict.items():
+        for new_ch_num in range(new_ch_max):
+            url = f"https://www.churchofjesuschrist.org/study/scriptures/" \
+                  f"bofm/{new_book}/{new_ch_num + 1}?lang={new_lang}"
+            try:
 
+                ch_dict = get_chapter(url)
                 # creates folders which don't exist yet
-                if not os.path.exists(f"./{new_lang}/{new_book}/"):
-                    os.makedirs(f"./{new_lang}/{new_book}/")
-                with open(f"./{new_lang}/{new_book}/{new_ch_num + 1:02d}.json",
+                if not os.path.exists(f"./texts/{new_lang}/{new_book}/"):
+                    os.makedirs(f"./texts/{new_lang}/{new_book}/")
+                with open(f"./texts/{new_lang}/{new_book}/{new_ch_num + 1:02d}.json",
                           "w") as outfile:
                     outfile.write(json.dumps(ch_dict,
                                              indent=2,
                                              sort_keys=True,
                                              ensure_ascii=False))
+            except HTTPError as err:
+                print(err.code)
+
+def add_lang_to_database(lang_short, collection):
+    results = collection.find_one({"lang_short": lang_short})
+    if not results:
+        lang_long = input(f"Please input the long-form of {lang_short}: ")
+        lang_dict = {"lang_short": lang_short, "lang_long": lang_long}
+        collection.insert_one(lang_dict)
 
 
 input_string = input("Enter new language codes seperated by a blank space, "
@@ -116,43 +127,51 @@ input_string = input("Enter new language codes seperated by a blank space, "
 input_list = []
 if input_string.lower() != "none":
     input_list = input_string.split(" ")
-    print(input_list)
 
-is_refresh_old = input("Should all languages be refreshed? (Y/n): ") == "Y"
-print(is_refresh_old)
+is_refresh_old = input("Should all languages be refreshed in database from text folder? (Y/n): ") == "Y"
+
 
 # define the client, the database, and the collection
 # the database and the collection are created at first insert
 # if needed
-client = MongoClient("mongodb://jake:nobodyworshipdullexpression@157.245.137"
-                     ".233:27017/reader")
-local_client = MongoClient("mongodb://localhost:27017/reader")
+client = MongoClient("mongodb://jake:nobodyworshipdullexpression@157.245.137.233:27017/reader")
+
 with client:
     mydb = client.reader
-    chapters = mydb["chapters"]
-    chapters.delete_many({})
+    chapter_collection = mydb["chapters"]
+    language_collection = mydb["languages"]
+
     # get a list of language folders
     if is_refresh_old:
         lang_list = [x for x in os.walk('./texts/')][0][1]
     else:
         lang_list = input_list
-    print(lang_list)
 
     for lang in lang_list:
+        add_lang_to_database(lang, language_collection)
+
         for book, ch_max in book_code_dict.items():
             for ch_num in range(ch_max):
-                with open(f"./texts/{lang}/{book}/{ch_num + 1:02d}.json",
-                          "r") as infile:
-                    ch_dict = json.loads(infile.read())
-                    # eliminate weird empty verses
-                    while "" in ch_dict["verses"]:
-                        ch_dict["verses"].remove("")
-                    # TODO: add to add chapter part of program
-                    # add a meta key
-                    ch_dict["meta"] = {"lang": lang, "book": book,
-                                       "ch_num": (ch_num + 1)}
-                    chapters.insert_one(ch_dict)
-                    print(f"added{ch_dict['meta']}")
 
-    local_client.reader.chapters.delete_many({})
-    local_client.reader.chapters.insert_many(chapters.find())
+                results = chapter_collection.find_one({"meta": {"lang": lang,
+                                                                "book": book,
+                                                                "ch_num": (
+                                                                            ch_num + 1)}})
+                filename = f"./texts/{lang}/{book}/{ch_num + 1:02d}.json"
+                if not results:
+                    if not os.path.isfile(filename):
+                        add_new_languages(lang)
+                    with open(filename, "r") as infile:
+                        ch_dict = json.loads(infile.read())
+                        # TODO: add to add chapter part of program
+                        # eliminate weird empty verses
+                        while "" in ch_dict["verses"]:
+                            ch_dict["verses"].remove("")
+                        # add a meta key
+                        ch_dict["meta"] = {"lang": lang, "book": book,
+                                           "ch_num": (ch_num + 1)}
+                        # END TODO
+                        chapter_collection.insert_one(ch_dict)
+                        print(f"Added: {lang} {book} {ch_num + 1}")
+                else:
+                    print(f"Skipped: {lang} {book} {ch_num + 1}")
